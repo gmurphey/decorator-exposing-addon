a839c95d2b192931c4477a77f44b8c3149e47f18
Garrett Murphey <gmurphey@trovedata.com>
2017-04-17 14:38:07 -0400

    WIP

--- index.js
+++ index.js
@@ -2,5 +2,19 @@
 'use strict';
 
 module.exports = {
-  name: 'decorator-exposing-addon'
+  name: 'decorator-exposing-addon',
+
+  init: function() {
+    this._super.init && this._super.init.apply(this, arguments);
+
+    this.options = this.options || {};
+    this.options.babel = this.options.babel || {};
+    this.options.babel.optional = this.options.babel.optional || [];
+
+    if (this.options.babel.optional.indexOf('es7.decorators') === -1) {
+      this.options.babel.optional.push('es7.decorators');
+    }
+
+    this.options.babel.includePolyfill = true;
+  }
 };
